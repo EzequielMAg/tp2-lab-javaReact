9c6411e9661e1829eac51fc5f216ad3105a9de9b
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-08-30 13:32:07 -0300

    Spring boot project created with spring initializr

---
+++ src/main/java/com/webapp/tp2_lab_javareact/Tp2LabJavareactApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.tp2_lab_javareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2LabJavareactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2LabJavareactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/tp2_lab_javareact/Tp2LabJavareactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.tp2_lab_javareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2LabJavareactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
