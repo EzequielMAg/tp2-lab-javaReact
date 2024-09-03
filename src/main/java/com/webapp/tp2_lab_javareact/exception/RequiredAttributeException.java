ff861efecb9fbc19337f7debc60040c5c4217fd3
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-03 20:56:20 -0300

    Refactor createEmployee() method, add calculateAge() method, and create RequiredAttributeException
    - Refactor: Improved readability and structure of the createEmployee() method by adding inline comments and setting the creation date within the method.
    - Added: Implemented the calculateAge(LocalDate birthDate) method to calculate the employee's age, with validation checks for null or future birth dates.
    - Added: Created RequiredAttributeException to handle cases where mandatory attributes are missing, used within the calculateAge() method.

---
+++ src/main/java/com/webapp/tp2_lab_javareact/exception/RequiredAttributeException.java
@@ -0,0 +1,10 @@
+package com.webapp.tp2_lab_javareact.exception;
+
+import org.aspectj.apache.bcel.classfile.Module;
+
+public class RequiredAttributeException extends BussinessException {
+
+    public RequiredAttributeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/webapp/tp2_lab_javareact/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/webapp/tp2_lab_javareact/service/impl/EmployeeServiceImpl.java
@@ -7,12 +7,15 @@ import com.webapp.tp2_lab_javareact.exception.EntityAttributeExistsException;
 import com.webapp.tp2_lab_javareact.exception.EntityNotFoundException;
 //import jakarta.persistence.EntityNotFoundException;
 
+import com.webapp.tp2_lab_javareact.exception.RequiredAttributeException;
 import com.webapp.tp2_lab_javareact.mapper.EmployeeMapper;
 import com.webapp.tp2_lab_javareact.repository.EmployeeRepository;
 import com.webapp.tp2_lab_javareact.service.EmployeeService;
 import com.webapp.tp2_lab_javareact.tool.NotificationMessage;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @Service
@@ -24,6 +27,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.repository = repository;
     }
 
+    //region -----------  HTTP METHODS  -----------
     @Override
     public EmployeeDTO getEmployee(Long id) {
 
@@ -39,10 +43,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDTO createEmployee(EmployeeDTO employeDTO) {
+    public EmployeeDTO createEmployee(EmployeeDTO employeeDTO) {
 
-        String email = employeDTO.getEmail();
-        String documentNumber = employeDTO.getDocumentNumber();
+        String email = employeeDTO.getEmail();
+        String documentNumber = employeeDTO.getDocumentNumber();
 
         if (this.repository.existsByEmail(email)) {
             throw new EntityAttributeExistsException( NotificationMessage.emailAlreadyExists(email));
@@ -52,7 +56,31 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new EntityAttributeExistsException( NotificationMessage.dniAlreadyExists(documentNumber));
         }
 
-        Employee employee = this.repository.save( EmployeeMapper.dtoToEmployee(employeDTO) );
+        // Establece la fecha de creacion del empleado (responsabilidad del sistema)
+        employeeDTO.setCreationDate(LocalDate.now());
+
+        // Convierte el DTO a entidad y la guarda en la DB
+        Employee employee = EmployeeMapper.dtoToEmployee(employeeDTO);
+        employee = this.repository.save( employee );
+
+        // Convierte la entidad guardada de vuelta a DTO y la retorna
         return EmployeeMapper.employeeToDto(employee);
     }
+    //endregion
+
+    //region -----------  BUSINESS LAYER METHODS  -----------
+    public static int calculateAge(LocalDate birthDate) {
+        LocalDate currentDate = LocalDate.now();
+
+        if (birthDate == null) {
+            throw new RequiredAttributeException( NotificationMessage.BIRTH_DATE_IS_REQUIRED );
+        }
+
+        if(birthDate.isAfter(currentDate)) {
+            throw new IllegalArgumentException( NotificationMessage.INVALID_BIRTH_DATE );
+        }
+
+        return Period.between(birthDate, currentDate).getYears(); // Calcula la diferencia en años
+    }
+    //endregion
 }
