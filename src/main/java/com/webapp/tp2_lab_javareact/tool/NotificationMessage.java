fa0ab37822965c11c98166c7f1f3cddfc6b07b65
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-03 20:20:28 -0300

    Improved `NotificationMessage` class with centralized validation messages
    - Added constants for validation messages related to required attributes in the EmployeeDTO class.
    - Included messages for invalid attribute values such as invalid email and date of birth.
    - Modularized messages for existing attributes with methods for email and document number existence.
    - Added a method to handle employee not found messages.

--- src/main/java/com/webapp/tp2_lab_javareact/tool/NotificationMessage.java
+++ src/main/java/com/webapp/tp2_lab_javareact/tool/NotificationMessage.java
@@ -5,10 +5,21 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public final class NotificationMessage {
 
-    public String employeeNotFound(Long id) {
-        return "¡The employee with id " + id + " was not found!";
-    }
+    //region -----------  CONSTANTS: ATTRIBUTE MESSAGE REQUIRED  -----------
+    public static final String DOCUMENT_NUMBER_IS_REQUIRED = "The documentNumber is required.";
+    public static final String NAME_IS_REQUIRED = "The name is required.";
+    public static final String LASTNAME_IS_REQUIRED = "The lastName is required.";
+    public static final String EMAIL_IS_REQUIRED = "The email is required.";
+    public static final String BIRTH_DATE_IS_REQUIRED = "The birthDate is required.";
+    public static final String ENTRY_DATE_IS_REQUIRED = "The entryDate is required.";
+    //endregion
 
+    //region -----------  CONSTANTS: INVALID ATTRIBUTE MESSAGE  -----------
+    public static final String INVALID_EMAIL = "You must provide a valid email.";
+    public static final String INVALID_BIRTH_DATE = "The date of birth must be before the current date.";
+    //endregion
+
+    //region -----------  METHODS THAT RETURN A MESSAGE: ATTRIBUTE ALREADY EXISTS  -----------
     public String emailAlreadyExists(String email) {
         return "¡The email " + email + " already exists!";
     }
@@ -16,4 +27,12 @@ public final class NotificationMessage {
     public String dniAlreadyExists(String dni) {
         return "¡The documentNumber " + dni + " already exists!";
     }
+    //endregion
+
+    //region OTHERS METHODS THAT RETURN A MESSAGE
+    public String employeeNotFound(Long id) {
+        return "¡The employee with id " + id + " was not found!";
+    }
+    //endregion
+
 }
