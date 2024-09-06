3bb467228de40b5eeed1f2da2058d4b8f72af95e
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-06 05:47:20 -0300

    Configure application.properties and add data.sql for initial data
    - Configured `application.properties` with settings for application name, server port, database, H2 console, and JPA/Hibernate.
    - Enabled database initialization to use `data.sql` for loading initial data.
    - Added `data.sql` file to insert 'labor concepts' into the database upon application startup.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO laboral_concept(id, maximum_hs, minimun_hs, laborable, name)
+VALUES (1, 8, 6, true, 'Turno Normal');
+
+INSERT INTO laboral_concept(id, maximum_hs, minimun_hs, laborable, name)
+VALUES (2, 6, 2, true, 'Turno Extra');
+
+INSERT INTO laboral_concept(id, maximum_hs, minimun_hs, laborable, name)
+VALUES (3, null, null, false, 'Día Libre');
